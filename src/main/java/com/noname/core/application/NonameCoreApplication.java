c766994e3156373864d5f6a12608f89819b007ef
诗笺 <2954700422@qq.com>
2025-03-06 07:54:26 +0800

    增强Application的判断条件

--- src/main/java/com/noname/core/application/NonameCoreApplication.java
+++ src/main/java/com/noname/core/application/NonameCoreApplication.java
@@ -3,5 +3,11 @@ package com.noname.core.application;
 import android.app.Application;
 
 public class NonameCoreApplication extends Application {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (getClass().getSuperclass() != Application.class) {
+            throw new RuntimeException("this class is not my NonameCoreApplication");
+        }
+    }
 }
